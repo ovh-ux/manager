083c7fbf0388227cbcc3f10e0dc57014d47b39ea
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2021-05-19 23:01:43 +0530

    feat(pci): add notebooks
    Add basic folder structure
    
    Ref #ANAGER-6832
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/notebooks/onboarding/onboarding.constants.js
@@ -0,0 +1,12 @@
+export const GUIDES = [
+  {
+    id: 'documentation',
+    link: 'https://docs.ovh.com/gb/en/notebooks/',
+  },
+  {
+    id: 'troubleshooting',
+    link: 'https://docs.ovh.com/gb/en/notebooks/',
+  },
+];
+
+export default { GUIDES };
