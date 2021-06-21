dd173f99501902b44801e9aa2a637253a7660b75
frenauvh <florian.renaut@corp.ovh.com>
2021-06-21 10:25:39 +0200

    fix(billing): add missing dependencies
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/billing/src/billing.module.js
+++ packages/manager/modules/billing/src/billing.module.js
@@ -4,6 +4,7 @@ import ngOvhUtils from '@ovh-ux/ng-ovh-utils';
 import ngRoute from 'angular-route';
 import ngTranslateAsyncLoader from '@ovh-ux/ng-translate-async-loader';
 import ngSanitize from 'angular-sanitize';
+import ovhAccountMigration from '@ovh-ux/manager-account-migration';
 import ovhManagerCore from '@ovh-ux/manager-core';
 import set from 'lodash/set';
 import uiBootstrap from 'angular-ui-bootstrap';
@@ -49,6 +50,7 @@ angular
     paymentCreditAdd,
     order,
     orders,
+    ovhAccountMigration,
     ovhAccountRefund,
     refunds,
     ngOvhExportCsv,
