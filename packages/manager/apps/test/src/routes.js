9055ea4d11734d82a2bfb5a0ab2c18d325d9930a
frenauvh <florian.renaut@corp.ovh.com>
2021-06-24 10:33:01 +0200

    fix(test): wip
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/test/src/routes.js
+++ packages/manager/apps/test/src/routes.js
@@ -1,29 +1,24 @@
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app', {
     url: '/?target',
-    redirectTo: (trans) => {
-      return trans
-        .injector()
-        .getAsync('test')
-        .then((test) => {
-          console.log(`:::: EXECUTE RedirectTo (${test})`);
-          let state = 'routeA';
-          switch (trans.params().target) {
-            case 'B':
-              state = 'routeA.routeB';
-              break;
-            case 'C':
-              state = 'routeA.routeB.routeC';
-              break;
-            default:
-            // do nothing
-          }
-          console.log('redirectTo:', state);
-          return { state };
-        });
-    },
     resolve: {
       test: () => Math.random(),
+      foo: () => {
+        console.log('foo is resolved');
+      },
+      redirect: ($state, $transition$) => {
+        switch ($transition$.params().target) {
+          case 'B':
+            $state.go('routeA.routeB');
+            break;
+          case 'C':
+            $state.go('routeA.routeB.routeC');
+            break;
+          default:
+            $state.go('routeA');
+            break;
+        }
+      },
     },
   });
 };
