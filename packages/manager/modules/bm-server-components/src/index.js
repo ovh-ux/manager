e0d429d2c00e37cb43355f5c64be8a011a55a7fc
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2021-07-07 14:06:35 +0530

    refactor(*): move dedicated server general info tile to a separate module
    ref #MANAGER-7282
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/index.js
+++ packages/manager/modules/bm-server-components/src/index.js
@@ -6,3 +6,4 @@ export { default as serverOsInstallImage } from './os-install/image';
 export { default as pollingService } from './polling/polling.service';
 export { default as serverTasks } from './tasks';
 export { default as serverInterventions } from './interventions';
+export { default as serverGeneralInfo } from './general-info-tile';
